dd797213ae6ea21340bf1a21abf9e4800cf887ba
Ananth <annanthpjose1996@gmail.com>
2026-07-24 22:22:44 +0530

    Fix Vercel API URL

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/v1/users";
-export const MOVIE_URL = "/api/v1/movies";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
+export const USERS_URL = `${BASE_URL}/api/v1/users`;
+export const MOVIE_URL = `${BASE_URL}/api/v1/movies`;
